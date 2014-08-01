5747d2e7931db927f7b2753196d97d7c8c27d147
Daniel Wiell <daniel.wiell@fao.org>
2014-08-01 11:31:40 +0200

    Updated code list size tests.

--- model/src/test/groovy/org/openforis/collect/android/collectadapter/DatabaseCodeListSizeDaoTest.groovy
+++ model/src/test/groovy/org/openforis/collect/android/collectadapter/DatabaseCodeListSizeDaoTest.groovy
@@ -8,6 +8,7 @@ import spock.lang.Specification
  * @author Daniel Wiell
  */
 class DatabaseCodeListSizeDaoTest extends Specification {
+    public static final NO_PARENT = null
     def codeListId = IdGenerator.nextId()
     def database = new ModelTestDatabase()
     def dao = new DatabaseCodeListSizeDao(database)
@@ -17,43 +18,36 @@ class DatabaseCodeListSizeDaoTest extends Specification {
     }
 
     def 'Can load root code list size'() {
-        3.times { insertItem(null) }
+        3.times { insertItem(NO_PARENT, 1) }
 
         expect: dao.codeListSize(codeListId, 1) == 3
     }
 
     def 'Child items are not counted when loading root code list size'() {
         def ids = [] as List<Integer>
-        3.times { ids << insertItem(null) }
-        insertItem(ids.first())
+        3.times { ids << insertItem(NO_PARENT, 1) }
+        insertItem(ids.first(), 2)
 
         expect: dao.codeListSize(codeListId, 1) == 3
     }
 
-    @Ignore
     def 'The size of provided level is returned, not the largest level overall'() {
         def ids = [] as List<Integer>
-        3.times { ids << insertItem(null) }
-        4.times { insertItem(ids.first()) }
+        3.times { ids << insertItem(NO_PARENT, 1) }
+        4.times { insertItem(ids.first(), 2) }
 
         expect: dao.codeListSize(codeListId, 1) == 3
     }
 
-    private int insertItem(Integer parentId) {
+    private int insertItem(Integer parentId, int level) {
         def id = IdGenerator.nextId()
         database.sql.executeInsert("""
-            INSERT INTO ofc_code_list(id, code_list_id, item_id, parent_id, sort_order, code)
-            VALUES ($id, $codeListId, $id, $parentId, 0, $id)
+            INSERT INTO ofc_code_list(id, code_list_id, item_id, parent_id, level, sort_order, code)
+            VALUES ($id, $codeListId, $id, $parentId, $level, 0, $id)
         """)
         return id
     }
 
 
-    @Ignore
-    def 'Can load external code list size'() {
-
-        when: true
-        then: false
-    }
 
 }

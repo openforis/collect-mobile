b65bd3f692434122a99e5110baa8ac5041a3a812
Daniel Wiell <daniel.wiell@fao.org>
2015-06-23 13:27:14 +0200

    Added missing description for attribute collections.

--- model/src/main/java/org/openforis/collect/android/viewmodel/UiAttributeCollectionDefinition.java
+++ model/src/main/java/org/openforis/collect/android/viewmodel/UiAttributeCollectionDefinition.java
@@ -11,7 +11,7 @@ public class UiAttributeCollectionDefinition extends Definition {
                                            Class<? extends UiAttribute> attributeType,
                                            UiAttributeDefinition attributeDefinition,
                                            boolean required) {
-        super(id, name, label, required);
+        super(id, name, label, null, attributeDefinition.description, null, required);
         this.attributeType = attributeType;
         this.attributeDefinition = attributeDefinition;
     }

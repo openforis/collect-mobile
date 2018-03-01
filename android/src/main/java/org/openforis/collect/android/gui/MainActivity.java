3b966f174bea8ce34435b09f8fdacfe98eb47729
fefrain <ferfrain@users.noreply.github.com>
2018-03-01 14:08:24 +0100

    Merge branch 'master' of https://github.com/openforis/collect-mobile
    # Conflicts:
    #	changes.txt
    #	gradle.properties

--- android/src/main/java/org/openforis/collect/android/gui/MainActivity.java
+++ android/src/main/java/org/openforis/collect/android/gui/MainActivity.java
@@ -126,7 +126,7 @@ public class MainActivity extends BaseActivity {
     }
 
     private void handleImportNewSurvey() {
-        SurveyListActivity.showImportDialog(this);
+        SurveyListActivity.startActivityAndShowImportDialog(this);
     }
 
     private void handleGoToDataEntryButtonClick(boolean surveySelected) {

911105b2d23b3974fe5d3ea50613cb70ee4f822f
Stefano Ricci <SteRiccio@users.noreply.github.com>
2023-11-07 23:04:25 +0100

    added record lock icons

--- android/src/main/java/org/openforis/collect/android/gui/SubmitDataToCollectActivity.java
+++ android/src/main/java/org/openforis/collect/android/gui/SubmitDataToCollectActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.openforis.collect.R;
@@ -236,7 +237,7 @@ public class SubmitDataToCollectActivity extends BaseActivity {
 
         @Override
         protected String doInBackground(String... args) {
-            String uploadUrl = remoteAddress + DATA_RESTORE_ENDPOINT;
+            String uploadUrl = StringUtils.removeEnd(remoteAddress, "/") + DATA_RESTORE_ENDPOINT;
             Log.d(LOG_TAG, "Uploading data file to: " + uploadUrl);
 
             try {
